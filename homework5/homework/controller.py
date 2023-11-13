a381daa7c79819d5585d0c974c87a47df1704656
Miughe <anthonydohogne7@gmail.com>
2023-11-13 01:54:53 +0000

    new code

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -1,62 +1,62 @@
 import pystk
+from itertools import product
+from homework.utils import PyTux
 import numpy as np
 
-def control(aim_point, current_vel):
+def control(aim_point, current_velocity):
     """
     Set the Action for the low-level controller
     :param aim_point: Aim point, in screen coordinate frame [-1..1]
-    :param current_vel: Current velocity of the kart
-    :return: a pystk.Action (set acceleration, brake, steer, drift)
+    :param current_velocity: Current velocity of the kart
+    :return: a pystk.Action (set acceleration, brake, steer, drift, nitro)
     """
+    # Constants
+    base_target_velocity = 40
+    steering_scale = 3.0
+    max_steering_angle = 1
+    drift_threshold = 0.90
+    nitro_threshold = 0.25
+    brake_threshold = 0.90
+
+    # Instantiate the action object
     action = pystk.Action()
 
-    # Target a constant velocity
-    target_velocity = 40  # You can adjust this value as needed
+    # Adaptive Target Velocity based on aim point (assuming sharper turn means closer aim point)
+    target_velocity = base_target_velocity * (1 - abs(aim_point[0]))
 
-    # Scale the aim_point to get normalized steering
-    steering_scale = 2.0  # You can tune this factor
-    target_steer = aim_point[0] * steering_scale
-    action.steer = np.clip(target_steer, -1, 1)  # Clip the steer angle to -1..1
+    # Dynamic Steering Adjustment based on current velocity
+    steering_scale = steering_scale * (base_target_velocity / (current_velocity + 1))
 
-    # Check for skidding
-    skid_threshold = 0.4  # Adjust this threshold as needed
-    if abs(target_steer) > skid_threshold:
-        action.drift = True  # Skid if the steering angle is too large
-    else:
-        action.drift = False
+    # Calculate the difference between the current velocity and the target velocity
+    velocity_difference = target_velocity - current_velocity
 
-    # Adjust acceleration and braking
-    if current_vel < target_velocity:
-        action.acceleration = 1  # Accelerate
-        action.brake = False
-    else:
-        action.acceleration = 0  # Maintain velocity
-        action.brake = True  # You may set this to True for sharper turns if needed
+    # Set acceleration to 1 if below target velocity, else 0
+    action.acceleration = 2.0 if velocity_difference > 0 else 0
 
-    # Use nitro for fast acceleration (optional)
-    use_nitro_threshold = 3.0  # Adjust this threshold as needed
-    if abs(target_steer) < use_nitro_threshold and current_vel > target_velocity * 0.6:
-        action.nitro = True
-    else:
-        action.nitro = False
+    # Calculate steering angle based on the aim point
+    steering_angle = aim_point[0] * steering_scale
+    steering_angle = max(min(steering_angle, max_steering_angle), -max_steering_angle)
+    action.steer = steering_angle
 
-    # Drift logic
-    if abs(target_steer) > 0.8:  # You can adjust this threshold as needed
-        action.drift = True  # Drift during sharp turns
-    else:
-        action.drift = False
+    # Advanced Drifting Logic
+    action.drift = abs(steering_angle) > drift_threshold and current_velocity > target_velocity
+
+    # Smart Braking Strategy
+    action.brake = abs(steering_angle) > brake_threshold and current_velocity > target_velocity
+
+    # Use nitro if going straight or almost straight
+    action.nitro = abs(steering_angle) < nitro_threshold
 
     return action
 
 if __name__ == '__main__':
-    from .utils import PyTux
     from argparse import ArgumentParser
 
     def test_controller(args):
         pytux = PyTux()
-        for t in args.track:
-            steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
-            print(steps, how_far)
+        for track_name in args.track:
+            steps, distance_traveled = pytux.rollout(track_name, control, max_frames=1000, verbose=args.verbose)
+            print(f'Track: {track_name}, Steps: {steps}, Distance: {distance_traveled}')
         pytux.close()
 
     parser = ArgumentParser()
